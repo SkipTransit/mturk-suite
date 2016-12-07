73a7034d10d1bd34315898be1f4dde0ab8b96ae6
Kadauchi <kadauchi@gmail.com>
2016-12-07 07:15:53 -0800

    TPE Menu will only be generated if called now.

--- global.js
+++ global.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   globaljs();
-  tpe_menu();
 });
 
 const globaljs = () => {
@@ -24,92 +23,9 @@ const globaljs = () => {
   });
 };
 
-const tpe_advanced = () => {
-  
-  
-};
-
-const _tpe = () => {
-  $('#tpe_menu').show();
-  
-  let html = '', c1 = 0, c2 = 0, tpe = 0;
-
-  chrome.storage.local.get('hits', (data) => {
-    let hits = data.hits || {};
-
-    const sorted = Object.keys(hits).sort( (a, b) => {return hits[a].viewed - hits[b].viewed;});
-
-    for (let i = 0; i < sorted.length; i ++) {
-      let key = sorted[i], color = '', source = '', autoapp = '', pend = false, status = 'allhits ';
-      
-      if (hits[key].status.match(/Paid|Approved/)) {
-        color = 'green'; status += 'app';
-      }
-      else if (hits[key].status.match(/Pending|Submitted/)) {
-        color = 'orange'; pend = true; status += 'pen';
-      }
-      else if (hits[key].status.match(/Rejected/)) {
-        color = 'red'; status += 'rej';
-      }
-      else if (hits[key].status.match(/Accepted|Previewed/)) {
-        status += 'vie';
-      }
-      if (hits[key].source) {
-        source = '<a href="' + hits[key].source + '" target="_blank" style="text-decoration: none;">&#x1F5D7;</a> ';
-      }
-      if (pend) {
-        if (hits[key].autoapp && hits[key].submitted) {
-          autoapp = _time_til_aa(hits[key].autoapp, hits[key].submitted);
-        }
-        else {
-          autoapp = 'There is no AA data for this HIT.';
-        }
-      }
-      
-     html +=
-       '<tr class="' + status + '">' +
-       '  <td><div>' + source + hits[key].reqname +'</div></td>' +
-       '  <td>' + hits[key].title + '</td>' +
-       '  <td style="width: 100px; text-align: center;">' + hits[key].reward + '</td>' +
-       '  <td style="width: 100px; text-align: center; color: ' + color + '; cursor: context-menu;" data-toggle="tooltip" data-placement="left" title="' + autoapp + '">' + hits[key].status.split(/\s/)[0] + '</td>' +
-       '</tr>'
-      ;
-    }
-  
-    $('#tbody').html(html);
-  });
-};
-
-function _time_til_aa (aa, sub) {
-  var willapp = 'This HIT will approve in ';
-  var autoapp = Number(aa);
-  var submit  = Number(sub);
-  var current = new Date().getTime() / 1000;
-  var remain  = Math.round(submit + autoapp - current);
-
-  if (remain > 0) {
-    var dd = Math.floor((remain / (60 * 60 * 24)));
-    var hh = Math.floor((remain / (60 * 60)) % 24);
-    var mm = Math.floor((remain / (60)) % 60);
-    var ss = remain % 60;
-        
-    willapp +=
-      (dd === 0 ? '' : dd + (dd > 1 ? ' days '    : ' day '))    +
-      (hh === 0 ? '' : hh + (hh > 1 ? ' hours '   : ' hour '))   +
-      (mm === 0 ? '' : mm + (mm > 1 ? ' minutes ' : ' minute ')) +
-      (ss === 0 ? '' : ss + (ss > 1 ? ' seconds ' : ' second '))
-    ;
-  }
-  else {
-    willapp = "This HIT should be approved.";
-  }
-  return willapp;
-}
-
-
 const tpe_menu = () => {
   $('body').append(
-    '<div id="tpe_menu" style="display: none; z-index: 99; position: fixed; width: 80%; height: 550px; left: 10%; top: 300px; margin-top: -250px; background-color: #373b44;">' +
+    '<div id="tpe_menu" style="z-index: 99; position: fixed; width: 80%; height: 550px; left: 10%; top: 300px; margin-top: -250px; background-color: #373b44;">' +
     '  <span style="float: right;">' +
     '    <button id="tpe_export" class="menu" type="button" style="height: 25px; margin-top: 5px;">Export</button>' +
     '    <button id="tpe_sync" class="menu" type="button" style="height: 25px; margin-top: 5px;">Sync</button>' +
@@ -133,12 +49,83 @@ const tpe_menu = () => {
     '  </div>' +
     '</div>'
   );
+  
+  chrome.storage.local.get('hits', (data) => {
+    let hits = data.hits || {}, html = '';
+    
+    const sorted = Object.keys(hits).sort( (a, b) => {return hits[a].viewed - hits[b].viewed;});
+
+    for (let i = 0; i < sorted.length; i ++) {
+      let key = sorted[i], color = '', source = '', autoapp = '', pend = false, status = 'allhits ';
+      
+      if (hits[key].status.match(/Paid|Approved/)) {
+        color = 'green'; status += 'app';
+      }
+      else if (hits[key].status.match(/Pending|Submitted/)) {
+        color = 'orange'; pend = true; status += 'pen';
+      }
+      else if (hits[key].status.match(/Rejected/)) {
+        color = 'red'; status += 'rej';
+      }
+      else if (hits[key].status.match(/Accepted|Previewed/)) {
+        status += 'vie';
+      }
+      if (hits[key].source) {
+        source = `<a href="${hits[key].source}" target="_blank">&#x1F5D7;</a> `;
+      }
+      if (pend) {
+        if (hits[key].autoapp && hits[key].submitted) {
+          autoapp = approve_when(hits[key].autoapp, hits[key].submitted);
+        }
+        else {
+          autoapp = 'There is no AA data for this HIT.';
+        }
+      }
+      
+     html +=
+       `<tr class="${status}">` +
+       `  <td><div>${source + hits[key].reqname}</div></td>` +
+       `  <td>${hits[key].title}</td>` +
+       `  <td style="width: 100px; text-align: center;">${hits[key].reward}</td>` +
+       `  <td style="width: 100px; text-align: center; color: ${color}; cursor: context-menu;" data-toggle="tooltip" data-placement="left" title="${autoapp}">${hits[key].status.split(/\s/)[0]}</td>'` +
+       `</tr>`
+      ;
+    }
+  
+    $('#tbody').html(html);
+  });
 };
 
+const approve_when = (aa, sub) => {
+  let willapp = 'This HIT will approve in ';
+  const autoapp = Number(aa);
+  const submit  = Number(sub);
+  const current = new Date().getTime() / 1000;
+  const remain  = Math.round(submit + autoapp - current);
+
+  if (remain > 0) {
+    const dd = Math.floor((remain / (60 * 60 * 24)));
+    const hh = Math.floor((remain / (60 * 60)) % 24);
+    const mm = Math.floor((remain / (60)) % 60);
+    const ss = remain % 60;
+        
+    willapp +=
+      (dd === 0 ? '' : dd + (dd > 1 ? ' days ' : ' day ')) +
+      (hh === 0 ? '' : hh + (hh > 1 ? ' hours ' : ' hour ')) +
+      (mm === 0 ? '' : mm + (mm > 1 ? ' minutes ' : ' minute ')) +
+      (ss === 0 ? '' : ss + (ss > 1 ? ' seconds ' : ' second '))
+    ;
+  }
+  else {
+    willapp = 'This HIT should be approved.';
+  }
+  return willapp;
+}
+
 $('html').on('click', '#tpe', () => {
-  _tpe();
+  tpe_menu();
 });
 
 $('html').on('click', '#tpe_close', () => {
-  $('#tpe_menu').hide();
+  $('#tpe_menu').remove();
 });
