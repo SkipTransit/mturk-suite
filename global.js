d65e3c569503bef939e629eb28203a72af4d6592
Kadauchi <kadauchi@gmail.com>
2016-12-07 23:30:23 -0800

    Added sync, export and beginning of Overview.

--- global.js
+++ global.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   globaljs();
 });
 
+let tpeexport = '';
+
 const globaljs = () => {
   chrome.storage.local.get('tpe', (data) => {
     const tpe = data.tpe || 0;
@@ -12,6 +14,16 @@ const globaljs = () => {
       `<span id="tpe" style="color: #008000; cursor: pointer;">$${tpe.toFixed(2)}<span>` +
       `</b>`
     );
+  });
+  
+  chrome.runtime.onMessage.addListener( (request) => {
+    if (request.msg == 'sync_tpe_running') {
+      progress(request.data.current, request.data.total);
+    }
+    if (request.msg == 'sync_tpe_done') {
+      $('#tpe_menu').remove();
+      tpe_menu();
+    }
   });
 
   chrome.storage.onChanged.addListener( (changes) => {
@@ -45,12 +57,11 @@ const tpe_menu = () => {
     
 
     '  <div id="Overview" class="tabcontent" style="display: block;">'+
-    '    <h3>Other two tabs are working.</h3>'+
-    '    <p>This tab is under construction.</p> '+
+    '    <h3>Loading Information.....</h3>'+
     '  </div>'+
     
     
-     '  <div id="Breakdown" class="tabcontent" style="height: 540px; overflow: auto;">' +
+    '  <div id="Breakdown" class="tabcontent" style="height: 540px; overflow: auto;">' +
     '    <table style="width: 100%;">' +
     '      <thead style="display; block;">' +
     '        <tr>' +
@@ -59,7 +70,7 @@ const tpe_menu = () => {
     '          <th style="text-align: center;">Reward</th>' +
     '        </tr>' +
     '      </thead>' +
-    '      <tbody id="breakdown_tbody" style="display; block; height: 500px; overflow: auto;;"></tbody>' +
+    '      <tbody id="breakdown_tbody" style="display; block; height: 500px; overflow: auto;"></tbody>' +
     '    </table>' +
     '  </div>' +
     
@@ -84,7 +95,52 @@ const tpe_menu = () => {
     const hits = data.hits || {};
     let breakdown = {}, breakdown_html = '', detailed_html = '';
     
+    // Overview
+    const total = Object.keys(hits).length
+    let submitted = 0, submitted_pe = 0;
+    let returned = 0;
+    let approved = 0, approved_pe = 0;
+    
+    for (let key in hits) {
+      if (hits[key].status.match(/Submitted|Paid|Approved|Pending/)) {
+        submitted ++;
+        submitted_pe += Number(hits[key].reward.replace(/[^0-9.]/g, ''));
+        
+        if (hits[key].status.match(/Paid|Approved/)) {
+          approved ++;
+          approved_pe += Number(hits[key].reward.replace(/[^0-9.]/g, ''));
+        };
+        
+        if (hits[key].status.match(/Submitted|Pending/)) {
+          const apped = is_approved(hits[key].autoapp, hits[key].submitted);
+          if (apped) {
+            approved ++;
+            approved_pe += Number(hits[key].reward.replace(/[^0-9.]/g, ''));
+          }
+        }
+      }
+    }
+    
+    $('#Overview').html(
+      `<div style="font-size: 20px; line-height: normal;">` +
+      `<br>` +
+      `<span><b>${total}</b> HITs have been viewed, submitted or returned today.</span>` +
+      `<br>` +
+      `<br>` +
+      //`<span><b>${submitted}</b> HITs have been submitted today for a total value of <b>$295.00</b>.</span>` +
+      `<span><b>${submitted}</b> HITs have been submitted today for a total value of <b>$${submitted_pe.toFixed(2)}</b>.</span>` +
+      `<br>` +
+      `<br>` +
+      //`<span><b>${approved}</b> HITs have been approved today for a total value of <b>$295.00</b>.</span>` +
+      `<span><b>${approved}</b> HITs have been approved today for a total value of <b>$${approved_pe.toFixed(2)}</b>.</span>` +
+      `</div>`
+    );
+    
     // Breakdown
+    tpeexport +=
+        `[b]Today\'s Projected Earnings: $${submitted_pe.toFixed(2)}[/b] (Exported from Mturk Suite)\n` +
+        `[spoiler=Today's Projected Earnings Full Details][table][tr][th][b]Requester[/b][/th][th][b]HITs[/b][/th][th][b]Projected[/b][/th][/tr]`;
+
     for (let key in hits) {
       if (hits[key].status.match(/(Submitted|Paid|Approved|Pending)/)) {
         const id = hits[key].reqid;
@@ -109,10 +165,20 @@ const tpe_menu = () => {
 
       breakdown_html +=
         `<tr>` +
+        //`  <td>reqname</td>` +
         `  <td>${breakdown[key].reqname}</td>` +
         `  <td style="width: 50px; text-align: right;">${breakdown[key].hits}</td>` +
+        //`  <td style="width: 50px; text-align: right;">$295.00</td>` +
         `  <td style="width: 50px; text-align: right;">$${breakdown[key].reward.toFixed(2)}</td>` +
         `</tr>`
+      ;
+      
+     tpeexport +=
+        `[tr]` +
+        `[td][url=https://www.mturk.com/mturk/searchbar?selectedSearchType=hitgroups&requesterId=${key}]${breakdown[key].reqname}[/url][/td]` +
+        `[td]${breakdown[key].hits}[/td]` +
+        `[td]$${breakdown[key].reward.toFixed(2)}[/td]` +
+        `[/tr]\n`
       ;
     }
     
@@ -149,16 +215,39 @@ const tpe_menu = () => {
       
      detailed_html +=
        `<tr class="${status}">` +
+       //`  <td><div>reqname</div></td>` +
+       //`  <td>title</td>` +
+       //`  <td style="width: 100px; text-align: center;">$295.00</td>` +
        `  <td><div>${source + hits[key].reqname}</div></td>` +
        `  <td>${hits[key].title}</td>` +
        `  <td style="width: 100px; text-align: center;">${hits[key].reward}</td>` +
-       `  <td style="width: 100px; text-align: center; color: ${color}; cursor: context-menu;" data-toggle="tooltip" data-placement="left" title="${autoapp}">${hits[key].status.split(/\s/)[0]}</td>'` +
+       `  <td style="width: 100px; text-align: center; color: ${color};" title="${autoapp}">${hits[key].status.split(/\s/)[0]}</td>` +
        `</tr>`
       ;
     }
   
     $('#detailed_tbody').html(detailed_html);
   });
+};
+
+const progress = (current, total) => {
+  const width = total === '???' ? 0 : current / total * 100;
+  $('#Overview, #Breakdown, #Detailed').html(
+    `<div style="text-align: center; padding: 5px;">` +
+    `  <h1>Syncing page ${current} of ${total}</h1>` +
+    `  <div style="width: 100%; height: 7px; border: 1px solid rgb(220, 140, 27); border-radius: 3px; overflow: hidden; margin-top: 3px; text-align: center; -webkit-user-select: none; -moz-user-select: none;">` +
+    `    <div style="height: 100%; background-color: rgb(220, 140, 27); user-select: none; width: ${width}%; background-image: linear-gradient(rgb(247, 223, 165) 0%, rgb(240, 193, 75) 100%);"></div>` +
+    `  </div>` +
+    `</div>`
+  );
+};
+
+const is_approved = (aa, sub) => {
+  const autoapp = Number(aa);
+  const submit  = Number(sub);
+  const current = new Date().getTime() / 1000;
+  const remain  = Math.round(submit + autoapp - current);
+  return remain > 0 ? false : true;
 };
 
 const approve_when = (aa, sub) => {
@@ -195,8 +284,26 @@ const tablinks = (id) => {
   $(`.${id}`).addClass('active');
 }
 
+const copyToClipboard1 = (template) => {
+  $('body').append(`<textarea id="copyToClipboard" style="opacity: 0;">${template}</textarea>`);
+  $('#copyToClipboard').select();
+  document.execCommand('Copy');
+  $('#copyToClipboard').remove();
+  alert(`Today's HITs Breakdown has been copied to your clipboard.`);
+};
+
+
 $('html').on('click', '#tpe', () => {
   tpe_menu();
+});
+
+$('html').on('click', '#tpe_export', () => {
+  copyToClipboard1(tpeexport);
+});
+
+$('html').on('click', '#tpe_sync', () => {
+  progress(1, '???');
+  chrome.runtime.sendMessage({msg: 'sync_tpe'});
 });
 
 $('html').on('click', '#tpe_close', () => {
