8782fee246717c78bbcf7baff27665baeced54ee
Kadauchi <kadauchi@gmail.com>
2016-12-13 15:41:47 -0800

    Reduced padding to match the site default.

--- pandalinks.js
+++ pandalinks.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const pandalinks = () => {
   for (let element of $('a[href^="/mturk/preview?groupId="]')) {
     $(element).before(
-      `<a href="${element.href.replace('preview?', 'previewandaccept?')}" target="_blank" style="padding-right: 20px;">Accept</a>`
+      `<a href="${element.href.replace('preview?', 'previewandaccept?')}" target="_blank" style="padding-right: 5px;">Accept</a>`
     );
   }
 };
