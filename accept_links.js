25e3269eba269e4b10d8ff5f8e5dfe58a1e58859
Kadauchi <kadauchi@gmail.com>
2016-12-13 17:16:50 -0800

    Changed file name.

--- accept_links.js
+++ accept_links.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if ($('a[href^="/mturk/preview?groupId="]').length) {
-    pandalinks();
-  }
+  ACCEPT_LINKS();
 });
 
-const pandalinks = () => {
+const ACCEPT_LINKS = () => {
   for (let element of $('a[href^="/mturk/preview?groupId="]')) {
     $(element).before(
       `<a href="${element.href.replace('preview?', 'previewandaccept?')}" target="_blank" style="padding-right: 5px;">Accept</a>`
