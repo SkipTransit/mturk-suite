260d5eaf2943ee64a3d3e074c6c55fe3f2140cfc
Kadauchi <kadauchi@gmail.com>
2016-12-22 15:43:16 -0800

    TO logging enabled.

--- background.js
+++ background.js
@@ -1,43 +1,43 @@
 let user = {}, dashboard = {}, tpe = {};
 let syncing_tpe = {tab: null, running: false};
 
-chrome.storage.local.get('user', (data) => {
+chrome.storage.local.get(`user`, (data) => {
   user = data.user || {goal: 20, dark: true};
   tpe.goal = user.goal;
 });
 
-chrome.storage.local.get('dashboard', (data) => {
-  dashboard = data.dashboard || {id: 'visit_your_dashboard', date: null, earn_hits: 0, earn_bonus: 0, earn_total: 0, earn_trans: 0, total_sub: 0, total_app: 0, total_rej: 0, total_pen: 0, today_sub: 0, today_app: 0, today_rej: 0, today_pen: 0};
+chrome.storage.local.get(`dashboard`, (data) => {
+  dashboard = data.dashboard || {id: `visit_your_dashboard`, date: null, earn_hits: 0, earn_bonus: 0, earn_total: 0, earn_trans: 0, total_sub: 0, total_app: 0, total_rej: 0, total_pen: 0, today_sub: 0, today_app: 0, today_rej: 0, today_pen: 0};
 });
 
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-  if (request.msg == 'user') {
+  if (request.msg == `user`) {
     user = request.data;
     chrome.storage.local.set({'user': user});
     tpe.goal = user.goal;
     chrome.storage.local.set({'tpe': tpe});
   }
-  if (request.msg == 'dashboard') {
+  if (request.msg == `dashboard`) {
     dashboard = request.data;
     chrome.storage.local.set({'dashboard': dashboard});
   }
-  if (request.msg == 'turkopticon') {
+  if (request.msg == `turkopticon`) {
     TODB_turkopticon(sender.tab.id, request.data);
   }
-  if (request.msg == 'hitexport') {
+  if (request.msg == `hitexport`) {
     TODB_hitexport(sender.tab.id, request.data);
   }
-  if (request.msg == 'sync_tpe') {
+  if (request.msg == `sync_tpe`) {
     sync_tpe(sender.tab.id, request.data);
   }
-  if (request.msg == 'send_mtc') {
+  if (request.msg == `send_mtc`) {
     SEND_MTC(sender.tab.id, request.data);
   }
-  if (request.msg == 'send_th') {
+  if (request.msg == `send_th`) {
     SEND_TH(sender.tab.id, request.data);
   }
-  if (request.msg == 'close_tpe_menu') {
-    chrome.tabs.sendMessage(sender.tab.id, {msg: 'close_tpe_menu'});
+  if (request.msg == `close_tpe_menu`) {
+    chrome.tabs.sendMessage(sender.tab.id, {msg: `close_tpe_menu`});
   }
 });
 
@@ -48,102 +48,148 @@ chrome.contextMenus.create({
   onclick: (info, tab) => {
     chrome.tabs.executeScript(tab.id, {
         frameId: info.frameId,
-        code: '// This code runs in one frame, specified via frameId \n' +
+        code: `// This code runs in one frame, specified via frameId \n` +
               `document.activeElement.value += '${dashboard.id}';`
     });
   }
 });
 
-// Adds context menu to paste worker id in input fields
+// Adds context menu to search mturk for highlighted text
 chrome.contextMenus.create({
   title: "Search Mturk",
   "type" : "normal",
   contexts: ["selection"],
   onclick: (info, tab) => {
-    chrome.tabs.create({url: `https://www.mturk.com/mturk/searchbar?selectedSearchType=hitgroups&searchWords=${info.selectionText}`})
+    chrome.tabs.create({url: `https://www.mturk.com/mturk/searchbar?selectedSearchType=hitgroups&searchWords=${info.selectionText}`});
   }
 });
 
 // Turkopticon IndexedDB
 let TODB;
-const request = indexedDB.open('TODB', 1);
+const request = indexedDB.open(`TODB`, 1);
 request.onsuccess = (event) => {
   TODB = event.target.result;
 };
 request.onupgradeneeded = (event) => {
   const TODB = event.target.result;
-  TODB.createObjectStore('requester', {keyPath: 'id'});
+  TODB.createObjectStore(`requester`, {keyPath: `id`});
 };
 
 const TODB_turkopticon = (tab, ids) => {
-  $.get(`https://turkopticon.ucsd.edu/api/multi-attrs.php?ids=${ids}`, (data) => {
-    const transaction = TODB.transaction(['requester'], 'readwrite');
-    const objectStore = transaction.objectStore('requester');
-
-    const json = JSON.parse(data);
-    for (let obj in json) {
-      if (json[obj]) {
-        json[obj].id = obj;
-        json[obj].edited = 1;
-        objectStore.put(json[obj]);
+  let grab = false;
+  const time = new Date().getTime();
+  const transaction = TODB.transaction([`requester`], `readwrite`);
+  const objectStore = transaction.objectStore(`requester`);
+  
+  for (let i = 0; i < ids.length; i ++) {
+    const request = objectStore.get(ids[i]);
+    request.onsuccess = (event) => {
+      if (request.result) {
+        if (request.result.edited < time - 21600000) {
+          grab = true;
+        }
       }
-    }
-    
-    const to = {};
-    for (let i = 0; i < ids.length; i ++) {
-      const request = objectStore.get(ids[i]);
-      request.onsuccess = (event) => {
-        if (request.result) {
-          to[ids[i]] = request.result;
-        }
-        else {
-          to[ids[i]] = {attrs: {comm: 0, fair: 0, fast: 0, pay: 0}, reviews: 0, tos_flags: 0};
-        }
-      };
-    }
-    
-    transaction.oncomplete = (event) => {
-      chrome.tabs.sendMessage(tab, {msg: 'turkopticon.js', data: to}); 
+      else {
+        grab = true;
+      }
     };
-  });
-};
+  }
+  
+  transaction.oncomplete = (event) => {
+    if (grab) {
+      $.get(`https://turkopticon.ucsd.edu/api/multi-attrs.php?ids=${ids}`, (data) => {
+        const transaction = TODB.transaction([`requester`], `readwrite`);
+        const objectStore = transaction.objectStore(`requester`);
 
-const TODB_hitexport = (tab, id) => {
-  const transaction = TODB.transaction(['requester']);
-  const objectStore = transaction.objectStore('requester');
-  const request = objectStore.get(id);
-
-  request.onsuccess = (event) => {
-    if (request.result) {
-      chrome.tabs.sendMessage(tab, {msg: 'hitexport.js', data: request.result});
+        const json = JSON.parse(data);
+        for (let obj in json) {
+          if (json[obj]) {
+            json[obj].id = obj;
+            json[obj].edited = time;
+            objectStore.put(json[obj]);
+          }
+        }
+    
+        const to = {};
+        for (let i = 0; i < ids.length; i ++) {
+          const request = objectStore.get(ids[i]);
+          request.onsuccess = (event) => {
+            if (request.result) {
+              to[ids[i]] = request.result;
+            }
+            else {
+              to[ids[i]] = {attrs: {comm: 0, fair: 0, fast: 0, pay: 0}, reviews: 0, tos_flags: 0};
+            }
+          };
+        }
+    
+        transaction.oncomplete = (event) => {
+          console.log('to requested');
+          chrome.tabs.sendMessage(tab, {msg: `turkopticon.js`, data: to}); 
+        };
+      });
     }
     else {
-      chrome.tabs.sendMessage(tab, {msg: 'hitexport.js', data: {attrs: {comm: 0, fair: 0, fast: 0, pay: 0}, reviews: 0, tos_flags: 0}});
+      const transaction = TODB.transaction([`requester`], `readwrite`);
+      const objectStore = transaction.objectStore(`requester`);
+
+      const to = {};
+      for (let i = 0; i < ids.length; i ++) {
+        const request = objectStore.get(ids[i]);
+        request.onsuccess = (event) => {
+          if (request.result) {
+            to[ids[i]] = request.result;
+          }
+          else {
+            to[ids[i]] = {attrs: {comm: 0, fair: 0, fast: 0, pay: 0}, reviews: 0, tos_flags: 0};
+          }
+        };
+      }
+    
+      transaction.oncomplete = (event) => {
+        console.log('to not requested');
+        chrome.tabs.sendMessage(tab, {msg: `turkopticon.js`, data: to}); 
+      };
     }
   };
 };
 
-const SEND_MTC = (tab, template) => {
+const TODB_hitexport = (tab, id) => {
+  const transaction = TODB.transaction([`requester`]);
+  const objectStore = transaction.objectStore(`requester`);
+  const request = objectStore.get(id);
+
+  request.onsuccess = (event) => {
+    if (request.result) {
+      chrome.tabs.sendMessage(tab, {msg: `hitexport.js`, data: request.result});
+    }
+    else {
+      chrome.tabs.sendMessage(tab, {msg: `hitexport.js`, data: {attrs: {comm: 0, fair: 0, fast: 0, pay: 0}, reviews: 0, tos_flags: 0}});
+    }
+  };
+};
+
+const SEND_MTC = (tab, message) => {
   $.get(`http://www.mturkcrowd.com/forums/4/?order=post_date&direction=desc`, (data) => {
     const $data = $(data);
-    const thread = $data.find('li[id^="thread-"]').eq(1).prop('id').replace('thread-', '');
-    const xfToken = $data.find('input[name="_xfToken"]').eq(0).val();
+    const thread = $data.find(`li[id^="thread-"]`).eq(1).prop(`id`).replace(`thread-`, ``);
+    const xfToken = $data.find(`input[name="_xfToken"]`).eq(0).val();
 
     $.post(`http://www.mturkcrowd.com/threads/${thread}/add-reply`, {
-      message_html: template,
+      message_html: message,
       _xfToken: xfToken
     });
   });
 };
 
-const SEND_TH = (tab, template) => {
+const SEND_TH = (tab, message) => {
   $.get(`https://turkerhub.com/forums/2/?order=post_date&direction=desc`, (data) => {
     const $data = $(data);
-    const thread = $data.find('li[id^="thread-"]').eq(1).prop('id').replace('thread-', '');
-    const xfToken = $data.find('input[name="_xfToken"]').eq(0).val();
+    const thread = $data.find(`li[id^="thread-"]`).eq(1).prop(`id`).replace(`thread-`, ``);
+    const xfToken = $data.find(`input[name="_xfToken"]`).eq(0).val();
 
     $.post(`https://turkerhub.com/threads/${thread}/add-reply`, {
-      message_html: template,
+      message_html: message,
       _xfToken: xfToken
     });
   });
@@ -153,7 +199,7 @@ const SEND_TH = (tab, template) => {
 let hits  = {};
 let requests = {};
 
-chrome.storage.local.get('hits', (data) => {
+chrome.storage.local.get(`hits`, (data) => {
   hits = data.hits || {};
   
   chrome.webRequest.onBeforeRequest.addListener( (data) => {
@@ -165,39 +211,39 @@ chrome.storage.local.get('hits', (data) => {
     }
     else {
       requests[data.requestId] = {
-        hitid : data.url.indexOf('hitId=') !== -1 ? data.url.split('hitId=')[1].split('&')[0] : null,
-        assignid : data.url.indexOf('assignmentId=') !== -1  ? data.url.split('assignmentId=')[1].split('&')[0] : null
+        hitid : data.url.indexOf(`hitId=`) !== -1 ? data.url.split(`hitId=`)[1].split(`&`)[0] : null,
+        assignid : data.url.indexOf(`assignmentId=`) !== -1  ? data.url.split(`assignmentId=`)[1].split(`&`)[0] : null
       };
     }
-  }, { urls: ['https://www.mturk.com/mturk/externalSubmit*'] }, ['requestBody']);
+  }, { urls: [`https://www.mturk.com/mturk/externalSubmit*`] }, [`requestBody`]);
   
   chrome.webRequest.onCompleted.addListener( (data) => {
-    if (data.statusCode == '200') {
+    if (data.statusCode == `200`) {
       if (requests[data.requestId].hitid) {
         const key = requests[data.requestId].hitid;
-        hits[key].status = 'Submitted';
+        hits[key].status = `Submitted`;
         hits[key].submitted = new Date().getTime() / 1000;
       }
       else {
         for (let key in hits) {
           if (hits[key].assignid === requests[data.requestId].assignid) {
-            hits[key].status = 'Submitted';
+            hits[key].status = `Submitted`;
             hits[key].submitted = new Date().getTime() / 1000;
           }
         }
       }
       update_tpe();
     }
-  }, { urls: ['https://www.mturk.com/mturk/externalSubmit*'] }, ['responseHeaders']);
+  }, { urls: [`https://www.mturk.com/mturk/externalSubmit*`] }, [`responseHeaders`]);
 
   chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    if (request.msg == 'sendhit') {
+    if (request.msg == `sendhit`) {
       ADD_HIT(request.data);
     }
   });
   
   chrome.runtime.onMessageExternal.addListener( (request, sender, sendResponse) => {
-    if (request.msg == 'sendhit') {
+    if (request.msg == `sendhit`) {
       ADD_HIT(request.data);
     }
   });
@@ -232,20 +278,20 @@ const sync_tpe = (tab) => {
   const scrape = (page) => {
     $.get(`https://www.mturk.com/mturk/statusdetail?encodedDate=${date}&pageNumber=${page}`, (data) => {
       const $data = $(data);
-      const url = $data.find('a:contains(Next)').eq(0).prop('href');
-      const err = $data.find('.error_title:contains(You have exceeded the maximum allowed page request rate for this website.)').length;
-      const act = $data.find('td:contains(You have no HIT activity on this day matching the selected status.)').length;
-      const last = $data.find('a:contains(Last)').length ? $data.find('a:contains(Last)').eq(0).prop('href').split('Number=')[1].split('&')[0] : null;
-      const $hits = $data.find('#dailyActivityTable').find('tr[valign="top"]');
+      const url = $data.find(`a:contains(Next)`).eq(0).prop(`href`);
+      const err = $data.find(`.error_title:contains(You have exceeded the maximum allowed page request rate for this website.)`).length;
+      const act = $data.find(`td:contains(You have no HIT activity on this day matching the selected status.)`).length;
+      const last = $data.find(`a:contains(Last)`).length ? $data.find(`a:contains(Last)`).eq(0).prop(`href`).split(`Number=`)[1].split(`&`)[0] : null;
+      const $hits = $data.find(`#dailyActivityTable`).find(`tr[valign="top"]`);
 
       if ($hits.length) {
         for (let i = 0; i < $hits.length; i++) {
-          const reqname = $hits.eq(i).find('td[class="statusdetailRequesterColumnValue"]').text().trim();
-          const reqid   = $hits.eq(i).find('a').prop('href').split('requesterId=')[1].split('&')[0];
-          const title   = $hits.eq(i).find('td[class="statusdetailTitleColumnValue"]').text().trim();
-          const reward  = $hits.eq(i).find('td[class="statusdetailAmountColumnValue"]').text().trim();
-          const status  = $hits.eq(i).find('td[class="statusdetailStatusColumnValue"]').text().trim();
-          const hitid   = $hits.eq(i).find('a').prop('href').split('hitId=')[1].split('&')[0];
+          const reqname = $hits.eq(i).find(`td[class="statusdetailRequesterColumnValue"]`).text().trim();
+          const reqid   = $hits.eq(i).find(`a`).prop(`href`).split(`requesterId=`)[1].split(`&`)[0];
+          const title   = $hits.eq(i).find(`td[class="statusdetailTitleColumnValue"]`).text().trim();
+          const reward  = $hits.eq(i).find(`td[class="statusdetailAmountColumnValue"]`).text().trim();
+          const status  = $hits.eq(i).find(`td[class="statusdetailStatusColumnValue"]`).text().trim();
+          const hitid   = $hits.eq(i).find(`a`).prop(`href`).split(`hitId=`)[1].split(`&`)[0];
 
           if (!hits[hitid]) {
             hits[hitid] = {
@@ -275,12 +321,12 @@ const sync_tpe = (tab) => {
         if (url) {
           page ++;
           scrape(page);
-          chrome.tabs.sendMessage(syncing_tpe.tab, {msg: 'sync_tpe_running', data: {current: page, total: last}});
+          chrome.tabs.sendMessage(syncing_tpe.tab, {msg: `sync_tpe_running`, data: {current: page, total: last}});
         }
         else {
           update_tpe();
           syncing_tpe.running = false;
-          chrome.tabs.sendMessage(syncing_tpe.tab, {msg: 'sync_tpe_done', data: {current: page, total: last}});
+          chrome.tabs.sendMessage(syncing_tpe.tab, {msg: `sync_tpe_done`, data: {current: page, total: last}});
         }
       }
       else if (err) {
@@ -290,7 +336,7 @@ const sync_tpe = (tab) => {
         hits = {};
         update_tpe();
         syncing_tpe.running = false;
-        chrome.tabs.sendMessage(syncing_tpe.tab, {msg: 'sync_tpe_done', data: {current: page, total: last}});
+        chrome.tabs.sendMessage(syncing_tpe.tab, {msg: `sync_tpe_done`, data: {current: page, total: last}});
       }
     });
   };
@@ -316,7 +362,7 @@ const update_tpe = () => {
       delete hits[key];
     }
     else if (!hits[key].status.match(/(Rejected|Accepted|Previewed|Returned)/)) {
-      tpe.tpe += Number(hits[key].reward.replace(/[^0-9.]/g, ''));
+      tpe.tpe += Number(hits[key].reward.replace(/[^0-9.]/g, ``));
     }
   }
 
@@ -328,10 +374,10 @@ const update_tpe = () => {
 const mturk_date = (time) => {
   const given = new Date(time);
   const utc = given.getTime() + (given.getTimezoneOffset() * 60000);
-  const offset = dst() === true ? '-7' : '-8';
+  const offset = dst() === true ? `-7` : `-8`;
   const amz = new Date(utc + (3600000 * offset));
-  const day = (amz.getDate()) < 10 ? '0' + (amz.getDate()).toString() : (amz.getDate()).toString();
-  const month = (amz.getMonth() + 1) < 10 ? '0' + (amz.getMonth() + 1).toString() : ((amz.getMonth() + 1)).toString();
+  const day = (amz.getDate()) < 10 ? `0` + (amz.getDate()).toString() : (amz.getDate()).toString();
+  const month = (amz.getMonth() + 1) < 10 ? `0` + (amz.getMonth() + 1).toString() : ((amz.getMonth() + 1)).toString();
   const year = (amz.getFullYear()).toString();
   return month + day + year;
 };
