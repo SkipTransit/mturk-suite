f62fb03ead4a7b738a6efc52b2a9e41695893491
Kadauchi <kadauchi@gmail.com>
2016-12-27 18:38:20 -0800

    Code more readable.

--- mturk.com/hit_capsule.js
+++ mturk.com/hit_capsule.js
@@ -5,12 +5,13 @@ document.addEventListener(`DOMContentLoaded`, () => {
 });
 
 const HIT_CAPSULE = () => {
-  const reqid = 
+  const reqid =
         $(`input[name="requesterId"]`).length ?
         $(`input[name="requesterId"]`).val():
-  $(`a[href^="/mturk/return?"]`).prop(`href`).match(/requesterId=(\w+)/) ?
+        $(`a[href^="/mturk/return?"]`).prop(`href`).match(/requesterId=(\w+)/) ?
         $(`a[href^="/mturk/return?"]`).prop(`href`).match(/requesterId=(\w+)/)[1]:
-  null;
+        null
+  ;
   const reqname = $(`.capsule_field_text`).eq(0).text().trim();
 
   const aa = $(`input[name="hitAutoAppDelayInSeconds"]`).eq(0).val();
