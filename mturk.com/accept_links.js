bb0a7053722f35cc37dce7c58dfd3fdda90d88d3
Kadauchi <kadauchi@gmail.com>
2016-12-30 20:56:48 -0800

    Removed arrow functions.

--- mturk.com/accept_links.js
+++ mturk.com/accept_links.js
@@ -1,11 +1,11 @@
-document.addEventListener(`DOMContentLoaded`, () => {
+document.addEventListener(`DOMContentLoaded`, function () {
   ACCEPT_LINKS();
 });
 
-const ACCEPT_LINKS = () => {
+function ACCEPT_LINKS () {
   for (let element of $(`a[href^="/mturk/preview?groupId="]`)) {
     $(element).before(
       `<a href="${element.href.replace(`preview?`, `previewandaccept?`)}" target="_blank" style="padding-right: 5px;">Accept</a>`
     );
   }
-};
+}
