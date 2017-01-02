6317b260221a6318ba200c009075563364129f17
Kadauchi <kadauchi@gmail.com>
2017-01-02 14:34:07 -0800

    Removed unneeded blank line.

--- mturk.com/accept_links.js
+++ mturk.com/accept_links.js
@@ -1,6 +1,5 @@
 const start_time = performance.now();
 
-
 for (let element of document.querySelectorAll(`a[href^="/mturk/preview?groupId="]`)) {
   element.insertAdjacentHTML(`beforebegin`,
     `<a href="${element.href.replace(`preview?`, `previewandaccept?`)}" target="_blank" style="padding-right: 10px;">Accept</a>`
