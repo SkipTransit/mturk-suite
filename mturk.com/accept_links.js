8084bd9f82e832074b699cf2ac54a1174210feaf
Kadauchi <kadauchi@gmail.com>
2017-01-02 15:27:32 -0800

    Changed structure.

--- mturk.com/accept_links.js
+++ mturk.com/accept_links.js
@@ -1,10 +1,11 @@
-const start_time = performance.now();
-
-for (let element of document.querySelectorAll(`a[href^="/mturk/preview?groupId="]`)) {
-  element.insertAdjacentHTML(`beforebegin`,
-    `<a href="${element.href.replace(`preview?`, `previewandaccept?`)}" target="_blank" style="padding-right: 10px;">Accept</a>`
-  );
+function ACCEPT_LINKS () {
+  for (let element of document.querySelectorAll(`a[href^="/mturk/preview?groupId="]`)) {
+    element.insertAdjacentHTML(`beforebegin`,
+      `<a href="${element.href.replace(`preview?`, `previewandaccept?`)}" target="_blank" style="padding-right: 10px;">Accept</a>`
+    );
+  }
 }
 
-const end_time = performance.now();
-console.log(`accept_links.js took ${(end_time - start_time)} milliseconds to complete.`);
+if (document.querySelector(`a[href^="/mturk/preview?groupId="]`)) {
+  ACCEPT_LINKS();
+}
