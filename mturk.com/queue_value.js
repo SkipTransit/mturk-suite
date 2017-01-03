e1c89f0bca1b4704bb19394f4dcded86682c91b1
Kadauchi <kadauchi@gmail.com>
2017-01-02 20:21:08 -0800

    Removed jquery.

--- mturk.com/queue_value.js
+++ mturk.com/queue_value.js
@@ -1,13 +1,8 @@
-document.addEventListener(`DOMContentLoaded`, function () {
-  if ($(`.nonboldsubnavclass[href*="myhits"]`).length) {
-    QUEUE_VALUE();
-  }
-});
-
 function QUEUE_VALUE () {
-  let total = 0;
-  for (let element of $(`.reward`)) {
-    total += Number($(element).text().replace(/[^0-9.]/g, ``));
-  }
-  $(`.title_orange_text_bold`).text(`Queue Value: $${total.toFixed(2)}`);
+  let total = [...document.getElementsByClassName(`reward`)].map(element => Number(element.textContent.replace(/[^0-9.]/g, ``))).reduce((a, b) => a + b, 0);
+  document.getElementsByClassName(`title_orange_text_bold`)[0].textContent = `Queue Value: $${total.toFixed(2)}`;
+}
+
+if (document.querySelector(`a[class="nonboldsubnavclass"][href*="myhits"]`) && document.getElementsByClassName(`title_orange_text_bold`)[0]) {
+  QUEUE_VALUE();
 }
