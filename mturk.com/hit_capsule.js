eda14c67b0469c30bd5961dff866f24594d9bf97
Kadauchi <kadauchi@gmail.com>
2017-01-02 21:39:25 -0800

    Removed jquery.

--- mturk.com/hit_capsule.js
+++ mturk.com/hit_capsule.js
@@ -1,20 +1,14 @@
-document.addEventListener(`DOMContentLoaded`, function () {
-  if ($(`input[name="isAccepted"]`).length) {
-    HIT_CAPSULE();
-  }
-});
-
 function HIT_CAPSULE () {
   const reqid =
-        $(`input[name="requesterId"]`).length ?
-        $(`input[name="requesterId"]`).val():
-        $(`a[href^="/mturk/return?"]`).prop(`href`).match(/requesterId=(\w+)/) ?
-        $(`a[href^="/mturk/return?"]`).prop(`href`).match(/requesterId=(\w+)/)[1]:
+        document.getElementsByName(`requesterId`)[0] ?
+        document.getElementsByName(`requesterId`)[0].value :
+        document.querySelector(`a[href^="/mturk/return?"]`).href.match(/requesterId=(\w+)/) ?
+        document.querySelector(`a[href^="/mturk/return?"]`).href.match(/requesterId=(\w+)/)[1]:
         null
   ;
-  const reqname = $(`.capsule_field_text`).eq(0).text().trim();
-``
-  const aa = $(`input[name="hitAutoAppDelayInSeconds"]`).val();
+  const reqname = document.getElementsByClassName(`capsule_field_text`)[0].textContent;
+
+  const aa = document.getElementsByName(`hitAutoAppDelayInSeconds`)[0].value;
   const d = Math.floor(aa / 86400);
   const h = Math.floor(aa / 3600 % 24);
   const m = Math.floor(aa / 60 % 60);
@@ -25,20 +19,17 @@ function HIT_CAPSULE () {
       (m > 0 ? `${m} minute${m > 1 ? `s` : ``} ` : ``)
   ;
 
-  if (aa === 0) {
-    aa_time = `0 seconds`;
-  }
-
-  $(`.capsule_field_text`).eq(0).parent().append(
-    `<td>` +
-    `  <img src="/media/spacer.gif" width="25" height="1" border="0">` +
-    `</td>` +
+  document.getElementsByClassName(`capsule_field_text`)[0].parentElement.insertAdjacentHTML(`beforeend`,
+    `<td><img src="/media/spacer.gif" width="25" height="1" border="0"></td>` +
     `<td align="right" valign="top" nowrap="" class="capsule_field_title">AA:&nbsp;&nbsp;</td>` +
-    `<td align="left" valign="top" nowrap="" class="capsule_field_text">${aa_time}</td>`
+    `<td align="left" valign="top" nowrap="" class="capsule_field_text">${aa !== 0 ? aa_time : `0 seconds`}</td>`
   );
-
   
-  $(`.capsule_field_text`).eq(0).html(
-    `<a href="/mturk/searchbar?selectedSearchType=hitgroups&${(reqid ? `requesterId=${reqid}` : `searchWords=${reqname.replace(/ /g, `+`)}`)}" target="_blank">${reqname}</a>`
-  );
+  document.getElementsByClassName(`capsule_field_text`)[0].innerHTML =
+    `<a href="/mturk/searchbar?selectedSearchType=hitgroups&${reqid ? `requesterId=${reqid}` : `searchWords=${reqname.replace(/ /g, `+`)}`}" target="_blank">${reqname}</a>`
+  ;
+}
+
+if (document.getElementsByName(`isAccepted`)[0]) {
+  HIT_CAPSULE();
 }
