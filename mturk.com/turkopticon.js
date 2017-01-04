589514e4e9a5f1fcbc358afc2d8874bc20aef2f7
Kadauchi <kadauchi@gmail.com>
2017-01-03 21:10:50 -0800

    Better code.

--- mturk.com/turkopticon.js
+++ mturk.com/turkopticon.js
@@ -13,19 +13,19 @@ function TURKOPTICON_WRITE (data) {
   function to (id) {
     const html = 
           `<div style="float:left;">` +
-          `  <div class="circle" style="background-color: ${color(data[id].attrs.pay)};">TO</div>` +
-          `  <div class="ratings">` +
+            `<div class="circle" style="background-color: ${color(data[id].attrs.pay)};">TO</div>` +
+            `<div class="ratings">` +
                rating(`Fair`, data[id].attrs.fair) +
                rating(`Fast`, data[id].attrs.fast) +
                rating(`Pay`, data[id].attrs.pay) +
                rating(`Comm`, data[id].attrs.comm) +
-          `    <br>` +
-          `    <div>Scores based on <a href="https://turkopticon.ucsd.edu/${id}" target="_blank">${data[id].reviews} review(s)</a></div>` +
-          `    <div>Terms of Service violation flags: ${data[id].tos_flags}</div>` +
-          `    <div>` +
-          `      <a href="https://turkopticon.ucsd.edu/report?requester[amzn_id]=${id}" target="_blank">Report your experience with this requester »</a>` +
-          `    </div>` +
-          `  </div>` +
+              `<br>` +
+              `<div>Scores based on <a href="https://turkopticon.ucsd.edu/${id}" target="_blank">${data[id].reviews} review(s)</a></div>` +
+              `<div>Terms of Service violation flags: ${data[id].tos_flags}</div>` +
+              `<div>` +
+                `<a href="https://turkopticon.ucsd.edu/report?requester[amzn_id]=${id}" target="_blank">Report your experience with this requester »</a>` +
+              `</div>` +
+            `</div>` +
           `</div>`;
     return html;
   }
@@ -35,23 +35,23 @@ function TURKOPTICON_WRITE (data) {
     if (rating > 0.01) {
       html =
         `<div style="display: table; width: 100%; cursor: default; height: 25px;">` +
-        `  <div class="capsule_field_title" style="display: table-cell; width:20%;">${type}:</div>` +
-        `  <div style="display: table-cell; width:20%;">${rating} / 5</div>` +
-        `  <div style="display: table-cell; width:60%;">` +
-        `    <div style="width: 100%; height: 12px; border: 1px solid; border-radius: 3px; overflow: hidden;">` +
-        `      <div style="height: 100%; background-color: ${color(rating)}; user-select: none; width: ${rating / 5 * 100}%;"></div>` +
-        `    </div>` +
-        `  </div>` +
+          `<div class="capsule_field_title" style="display: table-cell; width:20%;">${type}:</div>` +
+          `<div style="display: table-cell; width:20%;">${rating} / 5</div>` +
+          `<div style="display: table-cell; width:60%;">` +
+            `<div style="width: 100%; height: 12px; border: 1px solid; border-radius: 3px; overflow: hidden;">` +
+              `<div style="height: 100%; background-color: ${color(rating)}; user-select: none; width: ${rating / 5 * 100}%;"></div>` +
+            `</div>` +
+          `</div>` +
         `</div>`;
     }
     else {
       html =
         `<div style="display: table; width: 100%; cursor: default; height: 25px;">` +
-        `  <div class="capsule_field_title" style="display: table-cell; width:20%;">${type}:</div>` +
-        `  <div style="display: table-cell; width:20%;">No Data</div>` +
-        `  <div style="display: table-cell; width:60%;">` +
-        `    <div style="width: 100%; height: 12px; border: 1px solid; border-radius: 3px;"></div>` +
-        `  </div>` +
+          `<div class="capsule_field_title" style="display: table-cell; width:20%;">${type}:</div>` +
+          `<div style="display: table-cell; width:20%;">No Data</div>` +
+          `<div style="display: table-cell; width:60%;">` +
+            `<div style="width: 100%; height: 12px; border: 1px solid; border-radius: 3px;"></div>` +
+          `</div>` +
         `</div>`;
     }
     return html;
