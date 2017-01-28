2e2e46b2e6ebcf76ceaf302d3fb3ebd9a67a8047
Kadauchi <kadauchi@gmail.com>
2017-01-28 12:19:49 -0800

    Added accept next for worker.

---
+++ worker.mturk.com/accept_next.js
@@ -0,0 +1,19 @@
+function ACCEPT_NEXT () {
+  chrome.storage.local.get(`user`, function (data) {
+    const user = data.user || {accept_next: true};
+    $(`label:contains(Auto-accept Next Task)`).children()[0].checked = user.accept_next ? true : false;
+    //document.getElementsByName(`autoAcceptEnabled`)[0].checked = user.accept_next ? true : false;
+  });
+}
+
+if ($(`label:contains(Auto-accept Next Task)`)[0]) {
+  chrome.storage.onChanged.addListener( function (changes) {
+    for (let key in changes) {
+      if (key === `user`) {
+        ACCEPT_NEXT();
+      }
+    }
+  });
+
+  ACCEPT_NEXT();
+}
