c062586b6a855ed802c1e9998688974f9625ff1c
Kadauchi <kadauchi@gmail.com>
2017-02-06 16:07:37 -0800

    Added queue count + value for worker.mturk

---
+++ worker.mturk.com/queue_count_value.js
@@ -0,0 +1,8 @@
+function QUEUE_COUNT_VALUE () {
+  const total = [...document.getElementsByClassName(`reward-column`)].map(element => +element.textContent.replace(/[^0-9.]/g, ``)).reduce((a, b) => a + b, 0);
+  document.getElementsByClassName(`task-queue-header`)[0].children[0].children[0].textContent += ` - HITs: ${-- document.getElementsByClassName(`reward-column`).length} | Value: $${total.toFixed(2)}`;
+}
+
+if (document.querySelector(`[data-react-class="require('reactComponents/taskQueueTable/TaskQueueTable')['default']"]`)) {
+  QUEUE_COUNT_VALUE();
+}
