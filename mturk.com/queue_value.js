5a157dc055d7088fea91c92a4c6d47eef7800ec7
Kadauchi <kadauchi@gmail.com>
2017-03-05 12:52:44 -0800

    Moved

--- mturk.com/queue_value.js
+++
@@ -1,8 +0,0 @@
-function QUEUE_VALUE () {
-  const total = [...document.getElementsByClassName(`reward`)].map(element => +element.textContent.replace(/[^0-9.]/g, ``)).reduce((a, b) => a + b, 0);
-  document.getElementsByClassName(`title_orange_text_bold`)[0].textContent = `Queue Value: $${total.toFixed(2)}`;
-}
-
-if (document.querySelector(`a[class="nonboldsubnavclass"][href*="myhits"]`) && document.getElementsByClassName(`title_orange_text_bold`)[0]) {
-  QUEUE_VALUE();
-}
