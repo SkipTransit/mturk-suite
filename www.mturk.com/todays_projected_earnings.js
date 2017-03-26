63b1633efec55a9cd4522e34c1b44eaa4460e23b
Kadauchi <kadauchi@gmail.com>
2017-03-25 18:56:47 -0700

    Removed click to open menu.

--- www.mturk.com/todays_projected_earnings.js
+++ www.mturk.com/todays_projected_earnings.js
@@ -1,67 +1,43 @@
-function TODAYS_PROJECTED_EARNINGS () {
-  chrome.storage.local.get(`tpe`, function (data) {
-    const tpe = data.tpe || {tpe: 0, goal: 20};
-    TPE_WRITE(tpe.tpe, tpe.goal);
-  });
-}
-
-function TPE_WRITE (earnings, goal) {
-  const html =
-    `<div>` +
-      `<div id="tpe_goal_outer">` +
-        `<div id="tpe_goal_inner" style="width: ${Number(earnings) / Number(goal) * 100}%;"></div>` +
-      `</div>` +
-      `<div id="tpe_earnings">$${Number(earnings).toFixed(2)}/${Number(goal).toFixed(2)}<div>` +
-    `</div>`
-  ;
-  
-  if (document.getElementById(`tpe`)) {
-    document.getElementById(`tpe`).innerHTML = html;
-  }
-  else {
-    document.getElementById(`subtabs_and_searchbar`).insertAdjacentHTML(`afterbegin`, `<div id="tpe">${html}</div>`);
-  }
-}
-
-function TPE_MENU_WRITE () {
-  const html = `<iframe src="${chrome.runtime.getURL(`todays_hits_menu.html`)}" style="width: 100%; height: 100%;">`;
-  
-  if (document.getElementById(`tpe_menu`)) {
-    document.getElementById(`tpe_menu`).innerHTML = html;
-  }
-  else {
-    document.body.insertAdjacentHTML(`afterbegin`, `<div id="tpe_menu">${html}</div>`);
-  }
-}
-
-function TPE_MENU_CLOSE () {
-  document.body.removeChild(document.getElementById(`tpe_menu`));
-}
+const todaysProjectedEarnings = {
+  mts: {},
+  execute: function () {
+    console.log(`todaysProjectedEarnings.execute()`);
+    
+    const html =
+      `<div>
+        <div id="tpe_goal_outer">
+          <div id="tpe_goal_inner" style="width: ${+todaysProjectedEarnings.mts.tpe / +todaysProjectedEarnings.mts.goal * 100}%;"></div>
+        </div>
+        <div id="tpe_earnings">$${(+todaysProjectedEarnings.mts.tpe).toFixed(2)}/${(+todaysProjectedEarnings.mts.goal).toFixed(2)}<div>
+      </div>`
+    ;
+    
+    if (document.getElementById(`tpe`)) {
+      document.getElementById(`tpe`).innerHTML = html;
+    }
+    else {
+      document.getElementById(`subtabs_and_searchbar`).insertAdjacentHTML(
+        `afterbegin`,
+        `<div id="tpe">${html}</div>`
+      );
+    }
+  },
+};
 
 if (document.querySelector(`a[href="/mturk/beginsignout"]`)) {
-  chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
-    switch (request.msg) {
-      case `sync_tpe_done`: TPE_MENU_WRITE(); break;
-      case `close_tpe_menu`: TPE_MENU_CLOSE(); break;
-    }
-  });
-  
   chrome.storage.onChanged.addListener( function (changes) {
-    for (let key in changes) {
-      if (key === `tpe`) {
-        const newVal = changes[key].newValue, oldVal = changes[key].oldValue;
-        TPE_WRITE(newVal.tpe !== undefined ? newVal.tpe : oldVal.tpe, newVal.goal !== undefined ? newVal.goal : oldVal.goal);
+    if (changes.tpe) {
+      const newVal = changes.tpe.newValue, oldVal = changes.tpe.oldValue;
+      todaysProjectedEarnings.mts = {
+        tpe: newVal.tpe ? newVal.tpe : oldVal.tpe,
+        goal: newVal.goal ? newVal.goal : oldVal.goal
       }
+      todaysProjectedEarnings.execute();
     }
   });
   
-  document.addEventListener(`click`, function (event) {
-    const element = event.target;
-    
-    if (element.closest(`#tpe`)) {
-      TPE_MENU_WRITE();
-    }
+  chrome.storage.local.get(`tpe`, function (result) {
+    todaysProjectedEarnings.mts = result.tpe;
+    todaysProjectedEarnings.execute();
   });
-  
-  TODAYS_PROJECTED_EARNINGS();
 }
