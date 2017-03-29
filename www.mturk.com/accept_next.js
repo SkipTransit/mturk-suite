e26196543c426f2146bb9305366f2d85ddb2bab3
Kadauchi <kadauchi@gmail.com>
2017-03-29 00:50:26 -0700

    Updated. No longer forces uncheck.

--- www.mturk.com/accept_next.js
+++ www.mturk.com/accept_next.js
@@ -1,22 +1,28 @@
 const acceptNext = {
   mts: {},
-  execute: function () {
+  
+  execute () {
     console.log(`acceptNext.execute()`);
     
-    document.getElementsByName(`autoAcceptEnabled`)[0].checked = acceptNext.mts.acceptNext ? true : false;
-  }
-};
-
-if (document.getElementsByName(`autoAcceptEnabled`)[0]) {
-  chrome.storage.onChanged.addListener( function (changes) {
+    if (this.mts.acceptNext) {
+      document.getElementsByName(`autoAcceptEnabled`)[0].checked = true;
+    }
+  },
+  
+  storageLocalGet (result) {
+    acceptNext.mts = result.settings;
+    acceptNext.execute();
+  },
+  
+  storageOnChanged (changes) {
     if (changes.settings) {
       acceptNext.mts = changes.settings.newValue;
       acceptNext.execute();
     }
-  });
+  }
+};
 
-  chrome.storage.local.get(`settings`, function (result) {
-    acceptNext.mts = result.settings;
-    acceptNext.execute();
-  });
+if (document.getElementsByName(`autoAcceptEnabled`)[0]) {
+  chrome.storage.local.get(`settings`, acceptNext.storageLocalGet);
+  chrome.storage.onChanged.addListener(acceptNext.storageOnChanged);
 }
