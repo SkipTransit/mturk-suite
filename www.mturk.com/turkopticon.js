bc2dd6c2c61a0630426f3031ce58f0d869a6c95b
Kadauchi <kadauchi@gmail.com>
2017-04-01 11:58:02 -0700

    Code changes. Removed unneeded element.

--- www.mturk.com/turkopticon.js
+++ www.mturk.com/turkopticon.js
@@ -1,110 +1,87 @@
 const turkopticon = {
   mts: {},
-  execute: function () {
-    console.log(`turkopticon.execute()`);
+  
+  execute () {
     const ids = [];
     
-    for (let element of document.querySelectorAll(`a[href*="requesterId="]`)) {
-      if (element.href.match(/requesterId=(\w+)/)) {
-        ids.push(element.href.match(/requesterId=(\w+)/)[1]);
+    for (let elem of document.querySelectorAll(`a[href*="requesterId="]`)) {
+      if (elem.href.match(/requesterId=(\w+)/)) {
+        ids.push(elem.href.match(/requesterId=(\w+)/)[1]);
       }
     }
+    
     chrome.runtime.sendMessage({
       type: `turkopticon`,
       message: ids
     });
   },
-  draw: function (to) {
-    console.log(`turkopticon.draw()`);
-    turkopticon.ratings = to;
-    
-    for (let element of document.getElementsByClassName(`requesterIdentity`)) {
-      const reqid = 
-        element.closest(`a[href*="requesterId="]`) ?
-        element.closest(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/)[1] :
-        element.closest(`td[bgcolor="#F0F6F9"]`).querySelector(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/) ?
-        element.closest(`td[bgcolor="#F0F6F9"]`).querySelector(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/)[1] :
+  
+  draw () {
+    for (let elem of document.getElementsByClassName(`requesterIdentity`)) {
+      const reqId =
+        elem.closest(`a[href*="requesterId="]`) ?
+        elem.closest(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/)[1] :
+        elem.closest(`td[bgcolor="#F0F6F9"]`).querySelector(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/) ?
+        elem.closest(`td[bgcolor="#F0F6F9"]`).querySelector(`a[href*="requesterId="]`).href.match(/requesterId=(\w+)/)[1] :
         null
       ;
 
-      if (element.parentNode === element.closest(`a[href*="requesterId="]`)) {
-        element.parentNode.insertAdjacentHTML(`beforebegin`, turkopticon.circle(reqid));
+      if (elem.parentNode === elem.closest(`a[href*="requesterId="]`)) {
+        elem.parentNode.insertAdjacentHTML(`beforebegin`, this.element(reqId));
       }
       else {
-        element.insertAdjacentHTML(`beforebegin`, turkopticon.circle(reqid));
+        elem.insertAdjacentHTML(`beforebegin`, this.element(reqId));
       }
     }
     
     if (document.getElementsByName(`requesterId`)[0] || document.querySelector(`a[href^="/mturk/return?"]`) && document.querySelector(`a[href^="/mturk/return?"]`).href.match(/requesterId=(\w+)/)) {
-      const reqid = 
+      const reqId =
         document.getElementsByName(`requesterId`)[0] ?
         document.getElementsByName(`requesterId`)[0].value :
         document.querySelector(`a[href^="/mturk/return?"]`).href.match(/requesterId=(\w+)/) ?
         document.querySelector(`a[href^="/mturk/return?"]`).href.match(/requesterId=(\w+)/)[1] :
         null;
     
-      document.getElementsByClassName(`capsule_field_text`)[0].insertAdjacentHTML(`beforebegin`, turkopticon.circle(reqid));
+      document.getElementsByClassName(`capsule_field_text`)[0].insertAdjacentHTML(`beforebegin`, this.element(reqId));
     }
   },
-  circle: function (id) {
-    const to = turkopticon.ratings[id];
-    const mts = turkopticon.mts;
-    
-    let style = ``;
-    let color = `mts-toNone`;
-    let to1Color = `rgba(160, 160, 160, 0.65)`;
-    let to2Color = `rgba(160, 160, 160, 0.65)`;
-    
-    if (mts.to) {
-      if (mts.to.to1.use && mts.to.to2.use) {
-        if (to.to1) {
-          const pay = to.to1.attrs.pay;
-          if (pay >= mts.to.to1.high) to1Color = `rgba(0, 255, 0, 0.65)`;
-          else if (pay >= mts.to.to1.good) to1Color = `rgba(255, 255, 0, 0.65)`;
-          else if (pay >= mts.to.to1.average) to1Color = `rgba(255, 140, 0, 0.65)`;
-          else if (pay >= mts.to.to1.low) to1Color = `rgba(255, 0, 0, 0.65)`;
-        }
-        if (to.to2) {
-          const pay = to.to2.recent.reward[1] > 0 ? (to.to2.recent.reward[0] / to.to2.recent.reward[1]) * 60 ** 2 : 0;
-          if (pay >= mts.to.to2.high) to2Color = `rgba(0, 255, 0, 0.65)`;
-          else if (pay >= mts.to.to2.good) to2Color = `rgba(255, 255, 0, 0.65)`;
-          else if (pay >= mts.to.to2.average) to2Color = `rgba(255, 140, 0, 0.65)`;
-          else if (pay >= mts.to.to2.low) to2Color = `rgba(255, 0, 0, 0.65)`;
-        }
-        style = `style="background: linear-gradient(90deg, ${to1Color} 50%, ${to2Color} 50%);"`;
-      }
-      else {
-        if (mts.to.to1.use && to.to1) {
-          const pay = to.to1.attrs.pay;
-          if (pay >= mts.to.to1.high) color = `mts-toHigh`;
-          else if (pay >= mts.to.to1.good) color = `mts-toGood`;
-          else if (pay >= mts.to.to1.average) color = `mts-toAverage`;
-          else if (pay >= mts.to.to1.low) color = `mts-toLow`;
-        }
-        else if (mts.to.to2.use && to.to2) {
-          const pay = to.to2.recent.reward[1] > 0 ? (to.to2.recent.reward[0] / to.to2.recent.reward[1]) * 60 ** 2 : 0;
-          if (pay >= mts.to.to2.high) color = `mts-toHigh`;
-          else if (pay >= mts.to.to2.good) color = `mts-toGood`;
-          else if (pay >= mts.to.to2.average) color = `mts-toAverage`;
-          else if (pay >= mts.to.to2.low) color = `mts-toLow`;
-        }
-        style = `class="${color}"`;
-      }
+  
+  element (id) {
+    const to = this.ratings[id];
+    const mts = this.mts;
+        
+    function colors (pay, rate) {
+      if (pay >= rate.high) return `rgba(0, 255, 0, 0.65)`;
+      if (pay >= rate.good) return `rgba(255, 255, 0, 0.65)`;
+      if (pay >= rate.average) return `rgba(255, 140, 0, 0.65)`;
+      if (pay >= rate.low) return `rgba(255, 0, 0, 0.65)`;
+      return `rgba(160, 160, 160, 0.65)`;
     }
     
-    const html = 
-      `<mts-to>
-        <mts-to-circle ${style}>TO</mts-to-circle>
+    const to1Pay = to.to1 ? to.to1.attrs.pay : null;
+    const to1Color = colors(to1Pay, mts.to.to1);
+            
+    const to2Pay = to.to2 ? to.to2.recent.reward[1] > 0 ? (to.to2.recent.reward[0] / to.to2.recent.reward[1]) * 60 ** 2 : 0 : null;
+    const to2Color = colors(to2Pay, mts.to.to2);
+    
+    const style = 
+      mts.to.to1.use && mts.to.to2.use ?
+      `style="background: linear-gradient(90deg, ${to1Color} 50%, ${to2Color} 50%);"` :
+      `style="background: ${mts.to.to1.use ? to1Color : mts.to.to2.use ? to2Color : `rgba(160, 160, 160, 0.65)`}"`
+    ;
+    
+    const html =
+      `<mts-to ${style}>TO
         <mts-to-reviews>
-          ${turkopticon.attrTable(to)}
-          ${turkopticon.linkTable(id)}
+          ${this.attrTable(to)}
+          ${this.linkTable(to)}
         </mts-to-reviews>
       </mts-to>`
     ;
-    
     return html;
   },
-  attrTable: function (to) {
+  
+  attrTable (to) {
     const to1 = to.to1;
     const to2 = to.to2;
     const html =
@@ -116,7 +93,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>Pay:</mts-td>
           <mts-td>${to1 ? `${to1.attrs.pay} / 5` : `null`}</mts-td>
-
           <mts-td>Pay Rate:</mts-td>
           <mts-td>${to2 ? to2.recent.reward[1] > 0 ? `$${((to2.recent.reward[0] / to2.recent.reward[1]) * 60 ** 2).toFixed(2)}/hr` : `--/hr` : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.reward[1] > 0 ? `$${((to2.all.reward[0] / to2.all.reward[1]) * 60 ** 2).toFixed(2)}/hr` : `--/hr` : `null`}</mts-td>
@@ -124,7 +100,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>Fast:</mts-td>
           <mts-td>${to1 ? `${to1.attrs.fast} / 5` : `null`}</mts-td>
-
           <mts-td>Time Pending:</mts-td>
           <mts-td>${to2 ? to2.recent.pending > 0 ? `${(to2.recent.pending / 86400).toFixed(2)} days` : `-- days` : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.pending > 0 ? `${(to2.all.pending / 86400).toFixed(2)} days` : `-- days` : `null`}</mts-td>
@@ -132,7 +107,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>Comm:</mts-td>
           <mts-td>${to1 ? `${to1.attrs.comm} / 5` : `null`}</mts-td>
-
           <mts-td>Response:</mts-td>
           <mts-td>${to2 ? to2.recent.comm[1] > 0 ? `${Math.round(100 * to2.recent.comm[0] / to2.recent.comm[1])}% of ${to2.recent.comm[1]}` : `-- of 0` : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.comm[1] > 0 ? `${Math.round(100 * to2.all.comm[0] / to2.all.comm[1])}% of ${to2.all.comm[1]}` : `-- of 0` : `null`}</mts-td>
@@ -140,7 +114,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>Fair:</mts-td>
           <mts-td>${to1 ? `${to1.attrs.fair} / 5` : `null`}</mts-td>
-
           <mts-td>Recommend:</mts-td>
           <mts-td>${to2 ? to2.recent.recommend[1] > 0 ? `${Math.round(100 * to2.recent.recommend[0] / to2.recent.recommend[1])}% of ${to2.recent.recommend[1]}` : `-- of 0` : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.recommend[1] > 0 ? `${Math.round(100 * to2.all.recommend[0] / to2.all.recommend[1])}% of ${to2.all.recommend[1]}` : `-- of 0` : `null`}</mts-td>
@@ -148,7 +121,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>Reviews:</mts-td>
           <mts-td>${to1 ? `${to1.reviews}` : `null`}</mts-td>
-
           <mts-td>Rejected:</mts-td>
           <mts-td>${to2 ? to2.recent.rejected[0] : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.rejected[0] : `null`}</mts-td>
@@ -156,7 +128,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td>TOS:</mts-td>
           <mts-td>${to1 ? `${to1.tos_flags}` : `null`}</mts-td>
-
           <mts-td>TOS:</mts-td>
           <mts-td>${to2 ? to2.recent.tos[0] : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.tos[0] : `null`}</mts-td>
@@ -164,7 +135,6 @@ const turkopticon = {
         <mts-tr>
           <mts-td></mts-td>
           <mts-td></mts-td>
-
           <mts-td>Broken:</mts-td>
           <mts-td>${to2 ? to2.recent.broken[0] : `null`}</mts-td>
           <mts-td>${to2 ? to2.all.broken[0] : `null`}</mts-td>
@@ -173,35 +143,39 @@ const turkopticon = {
     ;
     return html;
   },
-  linkTable: function (id) {
-    const html = 
+  
+  linkTable (to) {
+    const html =
       `<mts-table class="mts-link-table">
         <mts-tr>
-          <mts-td><a target="_blank" href="https://turkopticon.ucsd.edu/${id}">View on TO 1</a></mts-td>
-          <mts-td><a target="_blank" href="https://turkopticon.info/requesters/${id}">View on TO 2</a></mts-td>
+          <mts-td><a target="_blank" href="https://turkopticon.ucsd.edu/${to.id}">View on TO 1</a></mts-td>
+          <mts-td><a target="_blank" href="https://turkopticon.info/requesters/${to.id}">View on TO 2</a></mts-td>
         </mts-tr>
-
         <mts-tr>
-          <mts-td><a target="_blank" href="https://turkopticon.ucsd.edu/report?requester[amzn_id]=${id}">Add review on TO 1</a></mts-td>
-          <mts-td><a target="_blank" href="https://turkopticon.info/reviews/new?rid=${id}">Add review on TO 2</a></mts-td>
+          <mts-td><a target="_blank" href="https://turkopticon.ucsd.edu/report?requester[amzn_id]=${to.id}">Add review on TO 1</a></mts-td>
+          <mts-td><a target="_blank" href="https://turkopticon.info/reviews/new?rid=${to.id}">Add review on TO 2</a></mts-td>
         </mts-tr>
       </mts-table>`
     ;
     return html;
-  }
+  },
+  
+  onMessage (request) {
+    switch (request.type) {
+      case `turkopticon`:
+        turkopticon.ratings = request.message;
+        turkopticon.draw();
+        break;
+    }
+  },
+  
+  storageLocalGet (result) {
+    turkopticon.mts = result.settings;
+    turkopticon.execute();
+  },
 };
 
 if (document.querySelector(`a[href*="requesterId="]`)) {
-  chrome.runtime.onMessage.addListener( function (request) {
-    switch (request.type) {
-      case `turkopticon`:
-        turkopticon.draw(request.message);
-        break;
-    }
-  });
-  
-  chrome.storage.local.get(`settings`, function (result) {
-    turkopticon.mts = result.settings ? result.settings : {};
-    turkopticon.execute();
-  });
+  chrome.runtime.onMessage.addListener(turkopticon.onMessage);
+  chrome.storage.local.get(`settings`, turkopticon.storageLocalGet);
 }
